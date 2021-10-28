72e9cee476d9997da51d0354f5c0101b4d831ec9
teekize <elvisteeka1@gmail.com>
2021-10-28 09:50:15 +0300

    created the boilerplate code for the mobile app

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>DoneWithIt</Text>
+      <Text>
+        You can now easily sell stuff that you own but would want to get a
+        shilling for it
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// this is styles object put all your stylings here
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
